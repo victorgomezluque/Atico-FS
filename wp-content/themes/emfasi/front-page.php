164bcf35b002e9205227047f7e24345e922dc88a
vgomez <v.gomez@emfasi.com>
2022-10-26 13:26:34 +0200

    home

--- wp-content/themes/emfasi/front-page.php
+++ wp-content/themes/emfasi/front-page.php
@@ -50,3 +50,6 @@ $sponsors = get_posts($args_sponsors);
         </div>
     </main><!-- #main -->
 </div><!-- #index-wrapper -->
+<!--<div class="instagram">
+    <img src="/wp-content/uploads/2022/10/instgram.png" alt="/wp-content/uploads/2022/10/instgram.png">
+</div>-->
