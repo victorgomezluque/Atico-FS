c6b6e46285792364a2195546cc55c614f6364b77
vgomez <v.gomez@emfasi.com>
2022-11-15 16:42:41 +0100

    sponsors

--- wp-content/themes/emfasi/front-page.php
+++ wp-content/themes/emfasi/front-page.php
@@ -48,5 +48,36 @@ $sponsors = get_posts($args_sponsors);
                 <a href="/contacto"> <img class="home-categories-contact-desktop" src="<?php the_field("imagen_contacto"); ?>" alt="wp-content/uploads/2022/10/Mini-banner.png"></a>
             </div>
         </div>
+        <!--
+        <div class="sponsors-home">
+            <h1 class="sponsors-home-title">SPONSORS</h1>
+            <div class="sponsors-home-grid">
+                <?php
+                /*
+                $args = array(
+                    'post_type' => 'sponsor',
+                    'post_status' => 'publish',
+                    'posts_per_page' => 4,
+                    'orderby' => 'title',
+                    'order' => 'ASC',
+                );
+
+                $loop = new WP_Query($args);
+                //print_r($loop);
+                while ($loop->have_posts()) : $loop->the_post(); ?>
+                    <div class="sponsor">
+                        <a href="<?php echo the_field('url_sponsor'); ?>">
+                            <img src="<?php echo the_field('imagen_sponsor'); ?>" alt="">
+                        </a>
+                    </div>
+
+                <?php
+
+                endwhile;
+                */
+                ?>
+            </div>
+            <a class="sponsors-home-link" href="/sponsors">Ver todos los sponsors</a>
+        </div>-->
     </main><!-- #main -->
 </div><!-- #index-wrapper -->
